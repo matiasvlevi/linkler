c8eebec5f00c71d5a037f8809392053bdb04f771
vlev <matiasvlevi@gmail.com>
2023-05-30 15:26:38 -0400

    bootstrapPermissions

--- linkler-cms/src/index.js
+++ linkler-cms/src/index.js
@@ -1,5 +1,37 @@
 'use strict';
 
+/**
+ * Enable find & findOne for all api content-types
+ */
+const boostrapPermissions = async (strapi) => {
+  const roles = await strapi
+    .service("plugin::users-permissions.role")
+    .find();
+
+  const _public = await strapi
+    .service("plugin::users-permissions.role")
+    .findOne(roles.filter((role) => role.type === "public")[0].id);
+
+  // Iterate over all api content-types
+  Object.keys(_public.permissions)
+    .filter(permission => permission.startsWith('api'))
+    .forEach(permission => {
+      const controller = Object.keys(_public.permissions[permission].controllers)[0];
+
+      // Enable find
+      _public.permissions[permission].controllers[controller].find.enabled = true;
+
+      // Enable findOne if exists
+      if (_public.permissions[permission].controllers[controller].findOne) 
+        _public.permissions[permission].controllers[controller].findOne.enabled = true;
+      
+    });
+
+  await strapi
+    .service("plugin::users-permissions.role")
+    .updateRole(_public.id, _public);
+};
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +48,7 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap({ strapi }) {
+    await boostrapPermissions(strapi);
+  },
 };
